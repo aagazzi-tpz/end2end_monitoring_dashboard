f95d206ebcaf43fa26a0c49002762ce8792c84d6
AGAZZI Anthony <anthony.agazzi@telespazio.com>
2025-03-14 01:14:35 +0100

    fix: :adhesive_bandage: Add fix to be timezone independent

--- modules/maas-cds/tests/conftest.py
+++ modules/maas-cds/tests/conftest.py
@@ -1,12 +1,13 @@
 """
-    Dummy conftest.py for maas_cds.
+Dummy conftest.py for maas_cds.
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
+If you don't know what this is for, just leave it empty.
+Read more about conftest.py under:
+- https://docs.pytest.org/en/stable/fixture.html
+- https://docs.pytest.org/en/stable/writing_plugins.html
 """
 
+import os
 import pytest
 from data.s5_data_test import *
 from data.s1_data_test import *
@@ -15,3 +16,8 @@ from data.s3_data_test import *
 from data.dataflow_stub import *
 from data.metrics_product_test import *
 from data.dd_attrs import *
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env():
+    os.environ["TZ"] = ""
